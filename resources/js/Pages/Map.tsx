c4b0a05438181b908c5fcdc51e14ff1ea2071f5f
sangij24001-admin <san-j24001@sangi.jp>
2026-02-24 17:31:37 +0900

    ズーム機能と複数地点のピンを表示する機能を実装（確認）

--- resources/js/Pages/Map.tsx
+++ resources/js/Pages/Map.tsx
@@ -3,16 +3,27 @@ import { APIProvider, AdvancedMarker, Map} from "@vis.gl/react-google-maps";
 
 // const {appName, customValue} = usePage().props;
 
-function DisplayMap() {
-    const position = {lat: 34.836439, lng: 138.176044};
+export default function DisplayMap() {
+    const location = [{id: 1, position: {lat: 34.836439, lng: 138.176044}}, {id: 2, position: {lat: 34.847132, lng: 138.183893}}];
+    const current_location = {lat: 34.847132, lng: 138.176044};
 
     return (
-        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-            <Map mapId = 'DEMO_MAP_ID' style={{width: '100vw', height: '100vh'}} center={position} zoom={15}>
-                <AdvancedMarker position={position}></AdvancedMarker>
+        <APIProvider
+            apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+        >
+            <Map
+                mapId = 'DEMO_MAP_ID'
+                style={{width: '100vw', height: '100vh'}}
+                defaultCenter={current_location}
+                defaultZoom={15}>
+                {location.map((loc) => (
+                    <AdvancedMarker
+                        key={loc.id}
+                        position={loc.position}
+                    >
+                    </AdvancedMarker>                
+                ))}
             </Map>
         </APIProvider>
     );
 }
-
-export default DisplayMap;
